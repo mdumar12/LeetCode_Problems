1b39505928ba2aaf286c78f053f7270ac520a38e
mdumar12 <111066402+mdumar12@users.noreply.github.com>
2023-08-13 18:36:34 +0530

    Create Final Value_of_Variable_After_Performing_Operations.java

---
+++ Final Value_of_Variable_After_Performing_Operations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(int i=0;i<operations.length;i++)
+        {
+            if(operations[i].equals("--X") || operations[i].equals("X--"))
+            {
+                x-=1;   
+            }
+            else
+            {
+                x+=1;
+            }
+        }
+        return x;
+        
+    }
+}
