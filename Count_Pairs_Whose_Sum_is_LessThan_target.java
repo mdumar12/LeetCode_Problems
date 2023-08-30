9fbb1ad486c1b2a666d1bbdf06feb5a47f8faa09
mdumar12 <111066402+mdumar12@users.noreply.github.com>
2023-08-31 01:02:33 +0530

    Create Count_Pairs_Whose_Sum_is_LessThan_target.java

---
+++ Count_Pairs_Whose_Sum_is_LessThan_target.java
@@ -0,0 +1,27 @@
+class Solution {
+  //Problem Number : 2824
+    public int countPairs(List<Integer> nums, int target) {
+        int cnt=0,l=nums.size();
+        for(int i=0;i<l;i++){
+            for(int j=i+1;j<l;j++){
+                if((nums.get(i)+nums.get(j))<target){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+        /*Collections.sort(nums);
+           int start =0;
+           int end = nums.size()-1,count =0;
+           while(start<end){
+                  if((nums.get(start)+nums.get(end)) < target){
+                      count = count+(end-start);
+                       start++;
+                  }else{
+                      end--;
+                  }
+           }
+            return count;*/
+        
+    }
+}
